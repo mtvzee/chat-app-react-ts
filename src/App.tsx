9e6f7ec006696305853893463fa217948d2811cf
mtvzee <pgzv01@gmail.com>
2022-12-01 19:19:44 +0900

    react-routerによるルーティングのページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 const App = () => {
   return (
-    <div>
-      Enter
-    </div>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
