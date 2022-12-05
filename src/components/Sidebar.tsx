02c9804017dbe552d91db6c30b7fce07ad6e65f4
mtvzee <pgzv01@gmail.com>
2022-12-05 19:08:04 +0900

    友達の追加をするための表示の雛形

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { AiOutlinePlus } from 'react-icons/ai';
 import {
   BsFillPersonPlusFill,
   BsThreeDotsVertical,
@@ -21,15 +22,24 @@ const Sidebar = () => {
         </div>
 
         {/* フレンドを追加 */}
-        <div className="flex items-center py-4 space-x-3">
-          <input
-            className="flex-auto  outline-none p-2 rounded-md text-black"
-            type="text"
-            placeholder="友達を追加(ユーザー名を入力)"
-          />
-          <button>
-            <BsFillPersonPlusFill className="w-8 h-8 hover:scale-110 transition" />
-          </button>
+        <div>
+          <div className="flex items-center py-4 space-x-3">
+            <input
+              className="flex-auto  outline-none p-2 rounded-md text-black"
+              type="text"
+              placeholder="友達を追加(ユーザー名を入力)"
+            />
+            <button>
+              <BsFillPersonPlusFill className="w-8 h-8 hover:scale-110 transition" />
+            </button>
+          </div>
+          <div className="flex  space-x-2 rounded-md py-3 hover:bg-[#283d55] cursor-pointer transition  relative group">
+            <Avatar />
+            <span className="text-lg">matsutani</span>
+            <button className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden group-hover:block animate-pulse">
+              <AiOutlinePlus className="w-10 h-10" />
+            </button>
+          </div>
         </div>
 
         {/* フレンド一覧 */}
