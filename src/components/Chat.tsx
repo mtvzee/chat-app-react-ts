63813ca4a81a872e3367594bdafacabc562a336b
mtvzee <pgzv01@gmail.com>
2022-12-06 18:01:28 +0900

    メッセージの削除ボタン追加

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -13,21 +13,27 @@ const Chat = () => {
       </header>
 
       <div className="flex-auto overflow-y-scroll">
-        <div className="w-[90%] mx-auto flex flex-col space-y-3 py-4">
+        <div className="w-[90%] mx-auto flex flex-col space-y-3 py-4 ">
           {/* チャット相手のテキスト欄 */}
-          <div className="flex items-start space-x-1 w-2/3">
+          <div className="flex items-start space-x-1 w-2/3 group">
             <Avatar width={40} height={40} />
             <p className="bg-[#f3f3f3] p-2 rounded-lg rounded-tl-none text-lg">
               こんにちは、私の名前は田中太郎です。よろしくお願いします。
               こんにちは、私の名前は田中太郎です。よろしくお願いします。
               こんにちは、私の名前は田中太郎です。よろしくお願いします。
             </p>
-            <span className="self-end text-sm">12:00</span>
+            <div className="self-end text-sm">
+              <button className="group-hover:block hidden">削除</button>
+              <span>12:00</span>
+            </div>
           </div>
 
           {/* 自分のテキスト欄 */}
-          <div className="flex items-end self-end space-x-1 w-2/3 text-justify">
-            <span className="self-end text-sm">12:00</span>
+          <div className="flex items-end self-end space-x-1 w-2/3 text-justify group">
+            <div className="self-end text-sm">
+              <button className="group-hover:block hidden ml-auto">削除</button>
+              <span>12:00</span>
+            </div>
             <p className="bg-[#93ed53] p-2 rounded-lg rounded-tr-none text-lg ">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque,
               corrupti! Totam esse illum officia. Voluptate quo, nesciunt,
