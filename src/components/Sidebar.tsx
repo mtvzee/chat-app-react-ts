7aaa2440081560030465c32903c1d945cd3dd893
mtvzee <pgzv01@gmail.com>
2022-12-06 18:01:50 +0900

    設定ドロップダウンメニューの追加

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,8 +16,14 @@ const Sidebar = () => {
             <Avatar width={40} height={40} />
             <span className="text-xl">takuya</span>
           </div>
-          <button>
-            <BsThreeDotsVertical className="w-6 h-6 hover:scale-110 transition" />
+          <button className="relative">
+            <BsThreeDotsVertical className="w-6 h-6 hover:scale-110 transition " />
+            <ul className="absolute top-10 right-0 w-52 bg-[#071d34] border border-white rounded-md overflow-hidden z-10">
+              <li className="p-3 hover:bg-[#283d55] border-b border-white">
+                プロフィールを変更
+              </li>
+              <li className="p-3 hover:bg-[#283d55]">ログアウト</li>
+            </ul>
           </button>
         </div>
 
