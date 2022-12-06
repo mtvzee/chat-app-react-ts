6dbea9c1626c9c8d407624fd2522bbae6310fa8e
mtvzee <pgzv01@gmail.com>
2022-12-06 18:52:38 +0900

    サイドバーの設定のトグル表示

--- src/components/SidebarHeader.tsx
+++ src/components/SidebarHeader.tsx
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import Avatar from './Avatar';
 
 const SidebarHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="flex items-center justify-between border-b border-[#263d54] py-4 ">
       <div className="flex items-center space-x-3 ">
         <Avatar width={40} height={40} />
         <span className="text-xl">takuya</span>
       </div>
-      <button className="relative">
+      <button className="relative" onClick={() => setIsOpen(!isOpen)}>
         <BsThreeDotsVertical className="w-6 h-6 hover:scale-110 transition " />
-        <ul className="absolute top-10 right-0 w-52 bg-[#071d34] border border-white rounded-md overflow-hidden z-10">
-          <li className="p-3 hover:bg-[#283d55] border-b border-white">
-            プロフィールを変更
-          </li>
-          <li className="p-3 hover:bg-[#283d55]">ログアウト</li>
-        </ul>
+        {isOpen && (
+          <ul className="absolute top-10 right-0 w-52 bg-[#071d34] border border-white rounded-md overflow-hidden z-10">
+            <li className="p-3 hover:bg-[#283d55] border-b border-white">
+              プロフィールを変更
+            </li>
+            <li className="p-3 hover:bg-[#283d55]">ログアウト</li>
+          </ul>
+        )}
       </button>
     </div>
   );
