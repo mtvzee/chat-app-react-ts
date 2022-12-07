e8f1f927598781a7188e3b8bed14c44c7b9c32d7
mtvzee <pgzv01@gmail.com>
2022-12-07 18:01:11 +0900

    友達を削除するモーダルの雛形

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { BsTrashFill } from 'react-icons/bs';
 import Avatar from './Avatar';
+import FriendDeleteModal from './FriendDeleteModal';
 
 const FriendList = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <ul className="overflow-y-scroll">
       <li className="flex justify-evenly space-x-2 rounded-md py-3 hover:bg-[#283d55] cursor-pointer relative group transition">
@@ -11,9 +14,13 @@ const FriendList = () => {
           <p className="text-sm">hello</p>
         </div>
         <span className="text-sm">12:00</span>
-        <button className="absolute bottom-2 right-2 hidden group-hover:block hover:scale-110 hover:text-red-500 transition">
+        <button
+          className="absolute bottom-2 right-2 hidden group-hover:block hover:scale-110 hover:text-red-500 transition"
+          onClick={() => setIsOpen(true)}
+        >
           <BsTrashFill className="w-4 h-4" />
         </button>
+        {isOpen && <FriendDeleteModal setIsOpen={setIsOpen} />}
       </li>
     </ul>
   );
