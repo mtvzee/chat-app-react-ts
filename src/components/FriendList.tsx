ab715c72f3c52b4700bbacc1e5d3ae77eb0d3d2f
mtvzee <pgzv01@gmail.com>
2022-12-12 16:58:02 +0900

    firestoreからのデータを表示

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -1,27 +1,63 @@
-import { useState } from 'react';
+import dayjs from 'dayjs';
+import { doc, onSnapshot, query, Timestamp } from 'firebase/firestore';
+import { useContext, useEffect, useState } from 'react';
 import { BsTrashFill } from 'react-icons/bs';
+import { AuthContext } from '../context/AuthContext';
+import { db } from '../firebase';
 import Avatar from './Avatar';
 import FriendDeleteModal from './FriendDeleteModal';
 
+type FriendListData = {
+  [key: string]: {
+    friendInfo: {
+      displayName: string;
+      photoURL: string;
+      uid: string;
+    };
+    latestMessage: string;
+    timestamp: Timestamp;
+  };
+};
+
 const FriendList = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [friendList, setFriendList] = useState<FriendListData>({});
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      doc(db, `friendList/${currentUser?.uid}`),
+      (snapshot) => {
+        setFriendList(snapshot.data() ?? {});
+      }
+    );
+    return unsub;
+  }, [currentUser?.uid]);
+
   return (
     <ul className="overflow-y-scroll">
-      <li className="flex justify-evenly space-x-2 rounded-md py-3 hover:bg-primary-hover cursor-pointer relative group transition">
-        <Avatar src={''} />
-        <div className="flex-auto">
-          <span className="text-lg">matsutani</span>
-          <p className="text-sm">hello</p>
-        </div>
-        <span className="text-sm">12:00</span>
-        <button
-          className="absolute bottom-2 right-2 hidden group-hover:block hover:scale-110 hover:text-red-500 transition"
-          onClick={() => setIsOpen(true)}
+      {Object.entries(friendList).map((friend) => (
+        <li
+          className="flex justify-evenly space-x-2 rounded-md py-3 hover:bg-primary-hover cursor-pointer relative group transition"
+          key={friend[0]}
         >
-          <BsTrashFill className="w-4 h-4" />
-        </button>
-        {isOpen && <FriendDeleteModal setIsOpen={setIsOpen} />}
-      </li>
+          <Avatar src={friend[1].friendInfo.photoURL} />
+          <div className="flex-auto">
+            <span className="text-lg">{friend[1].friendInfo.displayName}</span>
+            <p className="text-sm">{friend[1].latestMessage}</p>
+          </div>
+          <span className="text-sm">
+            {dayjs(friend[1]?.timestamp.toDate()).format('YYYY/MM/DD HH:mm:ss')}
+          </span>
+          <button
+            className="absolute bottom-2 right-2 hidden group-hover:block hover:scale-110 hover:text-red-500 transition"
+            onClick={() => setIsOpen(true)}
+          >
+            <BsTrashFill className="w-4 h-4" />
+          </button>
+          {isOpen && <FriendDeleteModal setIsOpen={setIsOpen} />}
+        </li>
+      ))}
     </ul>
   );
 };
