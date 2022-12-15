e22b176fd6ae6e5d6df4b9d22b075fde5faf1c32
mtvzee <pgzv01@gmail.com>
2022-12-15 19:40:05 +0900

    環境変数の修正

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,12 +14,12 @@ const firebaseConfig =
         appId: import.meta.env.VITE_FIREBASE_APP_ID,
       }
     : {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MASSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
+        apiKey: process.env.VITE_FIREBASE_API_KEY,
+        authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.VITE_FIREBASE_MASSAGING_SENDER_ID,
+        appId: process.env.VITE_FIREBASE_APP_ID,
       };
 
 export const app = initializeApp(firebaseConfig);
