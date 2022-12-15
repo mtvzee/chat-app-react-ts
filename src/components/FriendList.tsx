0ec63772d29190e6fa9b4458d21b3ef2307025e6
mtvzee <pgzv01@gmail.com>
2022-12-15 16:38:56 +0900

    friendListの並びを降順に

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -1,5 +1,12 @@
 import dayjs from 'dayjs';
-import { doc, onSnapshot, Timestamp } from 'firebase/firestore';
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  Timestamp,
+} from 'firebase/firestore';
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { SelectedUserContext } from '../context/SelectedUserContext';
@@ -36,29 +43,33 @@ const FriendList = () => {
 
   return (
     <ul className="overflow-y-scroll">
-      {Object.entries(friendList).map((friend) => (
-        <li
-          className="flex justify-evenly space-x-2 rounded-md py-3 hover:bg-primary-hover cursor-pointer relative group transition"
-          key={friend[0]}
-          onClick={() =>
-            dispatch({ type: 'CHANGE_USER', payload: friend[1].friendInfo })
-          }
-        >
-          <Avatar src={friend[1].friendInfo.photoURL} />
-          <div className="flex-auto">
-            <span className="text-lg">{friend[1].friendInfo.displayName}</span>
-            <p className="text-sm">{friend[1].latestContent}</p>
-          </div>
-          <span className="text-sm">
-            {dayjs(friend[1]?.timestamp?.toDate()).format('HH:mm')}
-          </span>
-          <FriendDeleteBtn
-            chatId={friend[0]}
-            displayName={friend[1].friendInfo.displayName}
-            photoURL={friend[1].friendInfo.photoURL}
-          />
-        </li>
-      ))}
+      {Object.entries(friendList)
+        .sort((a, b) => b[1].timestamp?.seconds - a[1].timestamp?.seconds)
+        .map((friend) => (
+          <li
+            className="flex justify-evenly space-x-2 rounded-md py-3 hover:bg-primary-hover cursor-pointer relative group transition"
+            key={friend[0]}
+            onClick={() =>
+              dispatch({ type: 'CHANGE_USER', payload: friend[1].friendInfo })
+            }
+          >
+            <Avatar src={friend[1].friendInfo.photoURL} />
+            <div className="flex-auto">
+              <span className="text-lg">
+                {friend[1].friendInfo.displayName}
+              </span>
+              <p className="text-sm">{friend[1].latestContent}</p>
+            </div>
+            <span className="text-sm text-gray-500">
+              {dayjs(friend[1]?.timestamp?.toDate()).format('HH:mm')}
+            </span>
+            <FriendDeleteBtn
+              chatId={friend[0]}
+              displayName={friend[1].friendInfo.displayName}
+              photoURL={friend[1].friendInfo.photoURL}
+            />
+          </li>
+        ))}
     </ul>
   );
 };
