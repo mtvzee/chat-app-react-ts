2bda109876da032879d7bb7773f7670574b36983
mtvzee <pgzv01@gmail.com>
2022-12-16 16:18:34 +0900

    400エラーの問題の修正-get関数にappを引数として渡す

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -30,7 +30,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+// export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
-export const db = getFirestore();
-export const storage = getStorage();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
