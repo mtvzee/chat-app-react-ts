90c4a70da780fe8d45b86a3c1d226ffab5e08e80
mtvzee <pgzv01@gmail.com>
2022-12-16 16:45:29 +0900

    不要なコードの削除

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,25 +1,7 @@
-import { getApp, getApps, initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-
-// const firebaseConfig = import.meta.env.PROD
-//   ? {
-//       apiKey: process.env.VITE_FIREBASE_API_KEY,
-//       authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-//       projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-//       storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-//       messagingSenderId: process.env.VITE_FIREBASE_MASSAGING_SENDER_ID,
-//       appId: process.env.VITE_FIREBASE_APP_ID,
-//     }
-//   : {
-//       apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//       authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//       projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//       storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//       messagingSenderId: import.meta.env.VITE_FIREBASE_MASSAGING_SENDER_ID,
-//       appId: import.meta.env.VITE_FIREBASE_APP_ID,
-//     };
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -30,8 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
